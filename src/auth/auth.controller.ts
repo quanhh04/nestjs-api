683136a2adae28da45eadbad6c3eef9ed3cbffd4
trantu\TUTRAN <tu.tran@fssc.com.vn>
2025-06-17 17:48:28 +0700

    add validator for req body

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDTO } from './dto'; // Import DTO for type safety
 
 @Controller('auth')
 export class AuthController {
@@ -10,10 +11,13 @@ export class AuthController {
 
     @Post('resgister')
     // register(@Req() req: Request) { // dung @Req() de lay request (la Decorator cua NestJS)
-    register(@Body() body: Body) { // dung @Body() de lay body request (la Decorator cua NestJS)
-        // body la Data Transfer Object (DTO) chua du lieu tu client gui len => phai chuan hoa du lieu ko cho phep client gui len cac truong khong hop le
         // req la Request Object chua toan bo thong tin ve request, bao gom headers, body, params, query, ...
-        console.log(body); // in ra body request
+    register(@Body() body: AuthDTO) { // dung @Body() de lay body request (la Decorator cua NestJS)
+        // body la Data Transfer Object (DTO) chua du lieu tu client gui len => phai chuan hoa du lieu ko cho phep client gui len cac truong khong hop le
+    // register(
+    //     @Body('email') email: string, // lay truong email tu body request
+    //     @Body('password') password: string, // lay truong password tu body request
+    // ) {
         return this.authServices.resgister();
     }
 
---
+++ src/auth/dto/index.ts
@@ -0,0 +1,2 @@
+// gom tất cả DTO từ các file khác nhau trong thư mục auth
+export * from './auth.dto';
